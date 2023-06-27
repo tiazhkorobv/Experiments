135a6ac88a6d424a3e8e40d2f3106f4cf8fcdccb
tiazhkorobv <vtzhkrob@yahoo.com>
2023-06-27 16:34:27 +0400

    added project

--- YT unhiden mouse/content.js
+++
@@ -1,7 +0,0 @@
-const videoPlayer = document.querySelector('.video-stream.html5-main-video');
-videoPlayer.addEventListener('mouseenter', () => {
-  videoPlayer.style.cursor = 'auto';
-});
-videoPlayer.addEventListener('mouseleave', () => {
-  videoPlayer.style.cursor = '';
-});
